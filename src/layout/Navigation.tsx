f2d54c099792c53d34743a31700422e7daddd259
jasonrht <Jason@Jasons-MacBook-Pro.local>
2022-06-13 02:33:28 +0200

    changed navbar

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,18 +1,22 @@
+import homeIcon from '../assets/home.png'
+
 export const Navigation = () => {
     return (
         <div className='h-full w-full bg-green-700 border-r-2 border-black'>
             <ul className='mx-2 sm:m-4 text-black text-center'>
-                <li className='mb-8 p-2 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:scale-[1.01] hover:translate-x-2 hover:opacity-[80%]'>
-                    <a href='/'>Home</a>
+                <li className='mb-14 mt-8'>
+                    <a href='/' className='py-2 px-10 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:opacity-[80%]'>
+                        Home
+                    </a>
                 </li>
-                <li className='my-4 p-2 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:scale-[1.01] hover:translate-x-2 hover:opacity-[80%]'>
-                    <a href='/'>Projects</a>
+                <li className='my-6'>
+                    <a href='/' className='py-2 px-10 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:opacity-[80%]'>Projects</a>
                 </li>
-                <li className='my-4 p-2 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:scale-[1.01] hover:translate-x-2 hover:opacity-[80%]'>
-                    <a href='/donate'>Donate</a>
+                <li className='my-6'>
+                    <a href='/donate' className='py-2 px-10 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:opacity-[80%]'>Donate</a>
                 </li>
-                <li className='my-4 p-2 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:scale-[1.01] hover:translate-x-2 hover:opacity-[80%]'>
-                    <a href='/'>Wallet Info</a>
+                <li className='my-6'>
+                    <a href='/' className='py-2 px-10 rounded-full border-2 border-black bg-gray-200 transition duration-100 ease-in-out hover:opacity-[80%]'>Wallet Info</a>
                 </li>
             </ul>
         </div>
